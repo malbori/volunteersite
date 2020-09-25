8c4f92eaca7f12a35f101fd8363f2be0f82ee650
Mahdi Al Bori <mahdialbori@gmail.com>
2020-09-25 14:39:56 -0400

    Removed accident folder addition

---
+++ Application/Operations/OperationCreate.cs
@@ -0,0 +1,19 @@
+using System;
+using MediatR;
+
+namespace Application.Operations
+{
+    public class OperationCreate
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
+    }
+}
