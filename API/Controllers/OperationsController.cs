9c039dd67e3be82b44cd35f2d3da9e87457e3b7f
Mahdi Al Bori <mahdialbori@gmail.com>
2020-09-28 14:45:28 -0400

    Added base controller

--- API/Controllers/OperationsController.cs
+++ API/Controllers/OperationsController.cs
@@ -8,48 +8,40 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OperationsController : ControllerBase
+    public class OperationsController : BaseController
     {
-        private readonly IMediator _mediator;
-        public OperationsController(IMediator mediator)
-        {
-            _mediator = mediator;
-
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<Operation>>> List()
         {
             // call Application layer to query data
-            return await _mediator.Send(new OperationList.Query());
+            return await Mediator.Send(new OperationList.Query());
         }
 
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Operation>> OperationDetails(Guid id)
         {
-            return await _mediator.Send(new OperationDetails.Query { Id = id });
+            return await Mediator.Send(new OperationDetails.Query { Id = id });
         }
 
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(OperationCreate.Command command)
         {
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Edit(Guid id, OperationEdit.Command command)
         {
             command.Id = id;
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            return await _mediator.Send(new OperationDelete.Command{Id = id});
+            return await Mediator.Send(new OperationDelete.Command{Id = id});
         }
     }
 }
