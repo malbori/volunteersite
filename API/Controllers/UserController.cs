cac978cad9eb9fae906d69123c0f31cda36269bc
Mahdi Al Bori <mahdialbori@gmail.com>
2020-09-28 16:17:56 -0400

    adding authorization blocks user from login endpoint..whoops. Added AllowAnonymous to user controller

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,11 +1,13 @@
 using System.Threading.Tasks;
 using Application.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     public class UserController : BaseController
     {
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
