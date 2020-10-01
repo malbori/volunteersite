80b4088d6f1d6d2a10d5f26c75dc45546a29f929
Mahdi Al Bori <mahdialbori@gmail.com>
2020-09-30 21:02:59 -0400

    Added server-side paging with list and offset

--- Application/Operations/OperationList.cs
+++ Application/Operations/OperationList.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -11,27 +14,66 @@ namespace Application.Operations
 {
     public class OperationList
     {
-        public class Query : IRequest<List<OperationDto>> { }
-
-        public class Handler : IRequestHandler<Query, List<OperationDto>>
+        public class OperationsEnvelope
         {
-            //constructer
+            public List<OperationDto> Operations { get; set; }
+            public int OperationsCount { get; set; }
+        }
+        public class Query : IRequest<OperationsEnvelope>
+        {
+            public Query(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
+            {
+                Limit = limit;
+                Offset = offset;
+                IsGoing = isGoing;
+                IsHost = isHost;
+                StartDate = startDate ?? DateTime.Now;
+            }
+            public int? Limit { get; set; }
+            public int? Offset { get; set; }
+            public bool IsGoing { get; set; }
+            public bool IsHost { get; set; }
+            public DateTime? StartDate { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, OperationsEnvelope>
+        {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-
-            public Handler(DataContext context, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
-
             }
 
-            public async Task<List<OperationDto>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<OperationsEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
-                var events = await _context.Operations.ToListAsync();
+                var queryable = _context.Operations
+                    .Where(x => x.Date >= request.StartDate)
+                    .OrderBy(x => x.Date)
+                    .AsQueryable();
 
-                return _mapper.Map<List<Operation>, List<OperationDto>>(events);
+                if (request.IsGoing && !request.IsHost)
+                {
+                    queryable = queryable.Where(x => x.UserOperations.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername()));
+                }
 
+                if (request.IsHost && !request.IsGoing)
+                {
+                    queryable = queryable.Where(x => x.UserOperations.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername() && a.IsHost));
+                }
+
+                var operations = await queryable
+                    .Skip(request.Offset ?? 0)
+                    .Take(request.Limit ?? 3).ToListAsync();
+
+                return new OperationsEnvelope
+                {
+                    Operations = _mapper.Map<List<Operation>, List<OperationDto>>(operations),
+                    OperationsCount = queryable.Count()
+                };
             }
         }
     }
